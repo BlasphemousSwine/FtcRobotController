3910f5d0d854eeba8ce34ed89654501adea822fa
BlasphemousSwine <timochristensen@icloud.com>
2022-02-04 14:33:48 -0700

    Message, I guess

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -2,9 +2,10 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 
 @TeleOp(name = "main")
-
+@Disabled
 public class Robot extends LinearOpMode {
 
     @Override
