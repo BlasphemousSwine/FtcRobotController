1b3414bf3e5ba50b15c8249349d67a72a934cd25
Timo Christensen | FTC 20108 <60856751+TimoChrist@users.noreply.github.com>
2022-02-04 17:55:16 -0700

    Git works on my laptop, too, right?

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -1,16 +1,5 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+public class Robot {
 
-@TeleOp(name = "main")
-@Disabled
-public class Robot extends LinearOpMode {
-
-    @Override
-    public void runOpMode () {
-        telemetry.update();
-    }
 }
-
